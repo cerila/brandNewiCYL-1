c6e02d7dfc085674c2fea232338aa5b778b712ff
alexgzhou <alexgzhou@163.com>
2014-11-20 14:12:20 +0800

    增加directive, 用于快速clear输入框中的内容
    .directive( "buttonClearInput", function () {
    return {
    restrict: "AE",
    scope: {
    input: "="  //这里可以直接用input获取父scope(包括兄弟元素)中ng-model的值,
    传递给本directive创建的isolate scope使用, template也属于当前isolate scope
    },
    // replace: true,   //使用replace之后, 本元素的click不能删除输入框中的内容, 原因大致可以理解为:
    父元素被替换后, scope.$apply没有执行对象
    template:"<button ng-if='input' class='button button-icon
    ion-android-close input-button padding-right'></button>",
    link: function (scope, element, attrs) {
    // console.log(scope.$id);
    // console.log(attrs.class);
    element.bind( "click", function () {
    scope.$apply(function () {
    // scope.item.searchContent = '';
    scope.input = "";
    });
    });
    }
    };
    })
    
    <button-clear-input input="item.searchContent"></button-clear-input>

--- www/js/icyl/directives.js
+++ www/js/icyl/directives.js
@@ -21,6 +21,27 @@ angular.module('icyl.directives', [])
     };
 }])
 
+.directive( "buttonClearInput", function () {
+    return {
+        restrict: "AE",
+        scope: {
+            input: "="  //这里可以直接用input获取父scope(包括兄弟元素)中ng-model的值, 传递给本directive创建的isolate scope使用, template也属于当前isolate scope
+        },
+        // replace: true,   //使用replace之后, 本元素的click不能删除输入框中的内容, 原因大致可以理解为: 父元素被替换后, scope.$apply没有执行对象
+        template:"<button ng-if='input' class='button button-icon ion-android-close input-button padding-right'></button>",
+        link: function (scope, element, attrs) {
+            // console.log(scope.$id);
+            // console.log(attrs.class);
+            element.bind( "click", function () {
+                scope.$apply(function () {
+                    // scope.item.searchContent = '';
+                    scope.input = "";
+                });
+            });
+        }
+    };
+})
+
 //获取并保存当前元素位置
 .directive( "elemPosition", ['$ionicPosition', '$window', '$timeout', function ($ionicPosition, $window, $timeout) {
     return {
